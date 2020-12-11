f54e3ac0cb164f44874010e2a5c3bcf0d9b7e842
Alice Dourado <alice.dourado@tecnico.ulisboa.pt>
2020-12-11 22:19:39 +0000

    day 11 improment (still not great)

--- 11/day11.py
+++ 11/day11.py
@@ -12,53 +12,48 @@ def printMap(mapa):
         print("".join(m))
     print()
 
-def sozinho(mapa, i, j):
+def searchNear(mapa, i, j, isCount):
+    count=0
     if (i>0):
-        if (mapa[i-1][j]=="#"): return False
-        if (j>0):
-            if (mapa[i-1][j-1]=="#"):return False
-        if (j<length-1):
-            if (mapa[i-1][j+1]=="#"):return False
+        if (mapa[i-1][j]=="#"): 
+            if isCount:count+=1
+            else: return False
+        if (j>0 and mapa[i-1][j-1]=="#"):
+            if isCount:count+=1
+            else: return False
+        if (j<length-1 and mapa[i-1][j+1]=="#"):
+            if isCount:count+=1
+            else: return False
     if (i<height-1):
-        if (mapa[i+1][j]=="#"): return False
-        if (j>0):
-            if (mapa[i+1][j-1]=="#"):return False
-        if (j<length-1):
-            if (mapa[i+1][j+1]=="#"):return False
+        if (mapa[i+1][j]=="#"): 
+            if isCount:count+=1
+            else:return False
+        if (j>0 and mapa[i+1][j-1]=="#"):
+            if isCount:count+=1
+            else: return False
+        if (j<length-1 and mapa[i+1][j+1]=="#"):
+            if isCount:count+=1
+            else: return False
     if (j>0):
-        if (mapa[i][j-1]=="#"): return False
+        if (mapa[i][j-1]=="#"): 
+            if isCount:count+=1
+            else:return False
     if (j<length-1):
-        if (mapa[i][j+1]=="#"): return False
-    return True
+        if (mapa[i][j+1]=="#"): 
+            if isCount:count+=1
+            else:return False
 
-def peopleAroud(mapa,i,j):
-    vizinhos=0
-    if (i>0):
-        if (mapa[i-1][j]=="#"): vizinhos+=1
-        if (j>0):
-            if (mapa[i-1][j-1]=="#"): vizinhos+=1
-        if (j<length-1):
-            if (mapa[i-1][j+1]=="#"): vizinhos+=1
-    if (i<height-1):
-        if (mapa[i+1][j]=="#"): vizinhos+=1
-        if (j>0):
-            if (mapa[i+1][j-1]=="#"): vizinhos+=1
-        if (j<length-1):
-            if (mapa[i+1][j+1]=="#"): vizinhos+=1
-    if (j>0):
-        if (mapa[i][j-1]=="#"): vizinhos+=1
-    if (j<length-1):
-        if (mapa[i][j+1]=="#"): vizinhos+=1
-    return vizinhos>=4
+    if isCount:return count>=4
+    else: return True
 
 def applyRules(oldMap):
     newMap=deepcopy(oldMap)
     for i in range(height):
         for j in range(length):
             seat = oldMap[i][j]
-            if (seat=="L" and sozinho(oldMap,i,j)):
+            if (seat=="L" and searchNear(oldMap,i,j, False)):
                 newMap[i][j]="#"
-            elif (seat == "#" and peopleAroud(oldMap,i,j)):
+            elif (seat == "#" and searchNear(oldMap,i,j, True)):
                 newMap[i][j]="L"
     
     return (newMap)
@@ -73,119 +68,61 @@ def countOccupied(mapa):
 oldMap=deepcopy(lines)
 while True:
     newMap = applyRules(oldMap)
+    #printMap(newMap)
     if (newMap==oldMap):
         print(countOccupied(newMap))
         break
     oldMap=deepcopy(newMap)
 
+down = lambda i : range(i+1,height)
+up = lambda i : range(i-1,-1,-1)
+left = lambda j: range(j-1, -1,-1)
+right = lambda j: range(j+1,length)
+ 
 #part 2
-def noVisiblePeople(mapa, i, j):
-    if (i>0):
-        for y in range(i-1,-1,-1):
-            if (mapa[y][j]=="#"):return False
-            elif (mapa[y][j]=="L"): break
-        if (j>0):
-            for y,x in zip(range(i-1, -1,-1), range(j-1, -1,-1)):
-                if (mapa[y][x]=="#"):return False
-                elif (mapa[y][x]=="L"): break
-        if (j<length-1):
-            for y,x in zip(range(i-1, -1,-1), range(j+1,length)):
-                if (mapa[y][x]=="#"):return False
-                elif (mapa[y][x]=="L"): break
-    if (i<height-1):
-        for y in range(i+1,height):
-            if (mapa[y][j]=="#"):return False
-            elif (mapa[y][j]=="L"): break
-        if (j>0):
-            for y,x in zip(range(i+1,height), range(j-1, -1,-1)):
-                if (mapa[y][x]=="#"):return False
-                elif (mapa[y][x]=="L"): break
-        if (j<length-1):
-            for y,x in zip(range(i+1, height), range(j+1, length)):
-                if (mapa[y][x]=="#"):return False
-                elif (mapa[y][x]=="L"):break
-    if (j>0):
-        for x in range(j-1,-1,-1):
-            if (mapa[i][x]=="#"):return False
-            elif (mapa[i][x]=="L"): break
-    if (j<length-1):
-        for x in range(j+1,length):
-            if (mapa[i][x]=="#"):return False
-            elif (mapa[i][x]=="L"): break
-    return True
-
-def peopleVisible(mapa, i, j):
+def searchLinesTudo(mapa, i, j, isCount):
+    vertical= [up(i),down(i)]
+    horizontal=[left(j),right(j)]
     occupied=0
-    if (i>0):
-        for y in range(i-1,-1,-1):
+    for d in vertical:
+        for y in d:
             if (mapa[y][j]=="#"):
-                occupied+=1
+                if (isCount): occupied+=1
+                else: return False
                 break
             elif (mapa[y][j]=="L"): break
-        
-        if (j>0):
-            for y,x in zip(range(i-1, -1,-1), range(j-1, -1,-1)):
+        for subDir in horizontal:
+            for y,x in zip(d, subDir):
                 if (mapa[y][x]=="#"):
-                    occupied+=1
+                    if (isCount): occupied+=1
+                    else: return False
                     break
                 elif (mapa[y][x]=="L"): break
-
-        if (j<length-1):
-            for y,x in zip(range(i-1, -1,-1), range(j+1,length)):
-                if (mapa[y][x]=="#"):
-                    occupied+=1
-                    break
-                elif (mapa[y][x]=="L"): break
-
-    if (i<height-1):
-        for y in range(i+1,height):
-            if (mapa[y][j]=="#"):
-                occupied+=1
-                break
-            elif (mapa[y][j]=="L"): break
-        if (j>0):
-            for y,x in zip(range(i+1,height), range(j-1, -1,-1)):
-                if (mapa[y][x]=="#"):
-                    occupied+=1
-                    break
-                elif (mapa[y][x]=="L"): break
-        if (j<length-1):
-            for y,x in zip(range(i+1, height), range(j+1, length)):
-                if (mapa[y][x]=="#"):
-                    occupied+=1
-                    break
-                elif (mapa[y][x]=="L"):break
-    if (j>0):
-        for x in range(j-1,-1,-1):
+    for d in horizontal:
+        for x in d:
             if (mapa[i][x]=="#"):
-                occupied+=1
+                if (isCount): occupied+=1
+                else: return False
                 break
             elif (mapa[i][x]=="L"): break
-    if (j<length-1):
-        for x in range(j+1,length):
-            if (mapa[i][x]=="#"):
-                occupied+=1
-                break
-            elif (mapa[i][x]=="L"): break
-    return occupied>=5
+    if (isCount): return occupied>=5
+    else: return True
 
 def applyRules2(oldMap):
     newMap=deepcopy(oldMap)
     for i in range(height):
         for j in range(length):
             seat = oldMap[i][j]
-            if (seat=="L" and noVisiblePeople(oldMap,i,j)):
+            if (seat=="L" and searchLinesTudo(oldMap,i,j, False)):
                 newMap[i][j]="#"
-            elif (seat == "#" and peopleVisible(oldMap,i,j)):
+            elif (seat == "#" and searchLinesTudo(oldMap,i,j, True)):
                 newMap[i][j]="L"       
     return (newMap)
 
 while True:
     newMap = applyRules2(lines)
     #printMap(newMap)
-
     if (newMap==lines):
         print(countOccupied(newMap))
         break
     lines=deepcopy(newMap)
-
