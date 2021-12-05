dbed4ef18f0ea8085dd90535ca2d275acb87b6d4
Alice Dourado <alice.dourado@tecnico.ulisboa.pt>
2021-12-05 14:31:54 +0000

    day 5

---
+++ 2021/05/2.py
@@ -0,0 +1,49 @@
+with open("input.txt") as file: lines = file.read().splitlines()
+
+sizeOfMap=1000
+diagram=[[0]*sizeOfMap for _ in range(sizeOfMap)]
+def printMap():
+    for l in diagram:
+        print(l)
+
+above2=0
+for l in lines:
+    # print(l)
+    fromm, to = l.split(" -> ")
+    x1, y1 = list(map(int,fromm.split(",")))
+    x2, y2 = list(map(int,to.split(",")))
+
+    if x1 == x2:
+        if y1>y2:
+            y1, y2 = y2, y1
+        for y in range(y1,y2+1):
+            diagram[y][x1] +=1
+            if diagram[y][x1]==2:
+                above2+=1
+
+    elif y1 == y2:
+        if x1>x2:
+            x1, x2 = x2, x1
+        for x in range(x1,x2+1):
+            diagram[y1][x] +=1
+            if diagram[y1][x]==2:
+                above2+=1
+
+    else:
+        if x1 > x2:
+            xRange = range(x1, x2-1, -1)
+        else: xRange = range(x1, x2+1)
+
+        if y1 > y2:
+            yRange = range(y1, y2-1, -1)
+        else: yRange = range(y1, y2+1) 
+
+        for x, y in zip(xRange, yRange):
+            diagram[y][x] +=1
+            if diagram[y][x]==2:
+                above2+=1
+
+    # printMap()
+
+# printMap()
+print(above2)                
