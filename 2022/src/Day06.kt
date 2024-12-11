8247b8f096f02de13a6bc48fac786cacd05c95a0
Alice Dourado <alice.dourado@revolut.com>
2024-12-11 00:59:07 +0000

    2022 files that I forgot to push

---
+++ 2022/src/Day06.kt
@@ -0,0 +1,26 @@
+import java.util.Stack
+
+fun main() {
+
+
+    fun part1(input: List<String>): String {
+        var result = ""
+        return result
+    }
+
+    fun part2(input: List<String>): String {
+        var result = ""
+        return result
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day06_test")
+    check(part1(testInput) == "CMZ")
+    //check(part2(testInput) == "MCD")
+
+    val input = readInput("Day06")
+    println("Part 1:")
+    println(part1(input))
+    println("Part 2:")
+    println(part2(input))
+}
---
+++ 2022/src/Day07.kt
@@ -0,0 +1,26 @@
+import java.util.Stack
+
+fun main() {
+
+
+    fun part1(input: List<String>): String {
+        var result = ""
+        return result
+    }
+
+    fun part2(input: List<String>): String {
+        var result = ""
+        return result
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day07_test")
+    check(part1(testInput) == "CMZ")
+    //check(part2(testInput) == "MCD")
+
+    val input = readInput("Day07")
+    println("Part 1:")
+    println(part1(input))
+    println("Part 2:")
+    println(part2(input))
+}
